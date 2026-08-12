5a527c70f337a668389f20571668949e1a468a31
Pau <Pau@10.16.11.206>
2026-08-12 11:11:44 -0600

    commit 3

--- src/pkg_modelo/Auto.java
+++ src/pkg_modelo/Auto.java
@@ -8,6 +8,40 @@ package pkg_modelo;
  *
  * @author Pau
  */
-public class Auto {
+public abstract class Auto {
+    protected int id;
+    protected String tipo;
+    protected String marca;
+    protected String anio;
+    protected String color; 
+
+    public Auto(int id, String tipo, String marca, String anio, String color) {
+        this.id = id;
+        this.tipo = tipo;
+        this.marca = marca;
+        this.anio = anio;
+        this.color = color;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getAnio() {
+        return anio;
+    }
+
+    public String getColor() {
+        return color;
+    }
     
+    public abstract String detalles();
 }
